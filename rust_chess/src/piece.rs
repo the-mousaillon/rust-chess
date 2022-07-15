be1a6866dd6a3c9355c505b39ca68bbd08df9858
theMoussaillon <nmorlier22@gmail.com>
2022-07-15 17:08:39 +0200

    [BOARD] - Add move generation skeletton

--- rust_chess/src/piece.rs
+++ rust_chess/src/piece.rs
@@ -1,6 +1,6 @@
 use super::chessbord::ChessBoard;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Color {
     Black,
     White,
@@ -19,6 +19,10 @@ pub enum Piece {
 
 fn get_piece_color(p: &impl PieceCommon) -> Option<Color> {
     Some(p.color())
+}
+
+fn get_piece_position(p: &impl PieceCommon) -> Option<Position> {
+    Some(p.position())
 }
 
 fn pprint_piece(p: &impl PieceCommon) -> &'static str {
@@ -44,6 +48,10 @@ impl Piece {
         multi_match!(self, get_piece_color, None)
     }
 
+    pub fn position(&self) -> Option<Position> {
+        multi_match!(self, get_piece_position, None)
+    }
+
     pub fn pprint(&self) {
         let emoj = self.emoji();
         print!("{}", emoj);
@@ -53,25 +61,148 @@ impl Piece {
         let empty = " ";
         multi_match!(self, pprint_piece, empty)
     }
+
+    pub fn is_empty(&self) -> bool {
+        match self {
+            Piece::Empty => true,
+            _ => false
+        }
+    }
 }
 
 pub type Position = (u8, u8);
 
+
+//// MOVES 
 #[derive(Clone, Debug)]
 pub enum Move {
     Move(Position, Position),
     Take(Position, Position),
     EnPassant(Position, Position),
-    KingsideCastle,
-    QueensideCastle,
+    KingsideCastle(Color),
+    QueensideCastle(Color),
+    Invalid
 }
 
+impl Move {
+    // This function should be called only for move or take moves. Castling and en passant would make this function pannic
+    pub fn new(p: &impl PieceCommon, board: &ChessBoard, to: Position) -> Self {
+        let dst = &board.board[to.0 as usize][to.1 as usize];
+        // If we try to move in our own piece, the move is invalid
+        if !dst.is_empty() && (dst.color().unwrap() == p.color()) {
+            return Move::Invalid
+        }
+        // Otherwise we either move or take
+        match dst {
+            Piece::Empty => Self::Move(p.position(), to),
+            p_ => Move::Take(p.position(), to),
+        }
+    }
+}
+
+fn diag_line_generic(p: &impl PieceCommon, board: &ChessBoard, n_travel: u8, pos_trans: impl Fn(u8) -> Position) -> Vec<Move> {
+    let mut moves = vec!();
+    for i in 0..n_travel {
+        let to = pos_trans(i);
+        let move_ = Move::new(p, board, to);
+        match move_ {
+            Move::Move(_, _) =>{
+                moves.push(move_);
+            },
+            Move::Take(_, _) => {
+                moves.push(move_);
+                break
+            },
+            Move::Invalid => {
+                break
+            }
+            _ => panic!("This case shoud never happn, check the code")
+        }
+    }
+    moves
+}
+
+fn line_left(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0, pos.1 - i)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn line_right(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0, pos.1 + i)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn line_up(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0 - i, pos.1)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn line_down(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0 + 1, pos.1)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn diag_left_up(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0 - i, pos.1 - i)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn diag_right_up(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0 - i, pos.1 + i)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn diag_left_down(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0 + i, pos.1 - i)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+fn diag_right_down(p: &impl PieceCommon, board: &ChessBoard) -> Vec<Move> {
+    let pos = p.position();
+    let n_travel = pos.1;
+    let pos_trans = |i: u8| {
+        (pos.0 + i, pos.1 + i)
+    };
+    diag_line_generic(p, board, n_travel, pos_trans)
+}
+
+/// 
 pub trait PieceCommon {
     fn new(p: Position, color: Color) -> Self;
 
     fn color(&self) -> Color;
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move>;
+    fn position(&self) -> Position;
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move>;
 
     fn emoji_repr(&self) -> &'static str;
 }
@@ -89,11 +220,16 @@ impl PieceCommon for Pawn {
             position: p,
         }
     }
+
     fn color(&self) -> Color {
         self.color.clone()
     }
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move> {
+    fn position(&self) -> Position {
+        self.position.clone()
+    }
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move> {
         vec![]
     }
 
@@ -118,12 +254,27 @@ impl PieceCommon for Knight {
             position: p,
         }
     }
+
     fn color(&self) -> Color {
         self.color.clone()
     }
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move> {
-        vec![]
+    fn position(&self) -> Position {
+        self.position.clone()
+    }
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move> {
+        // ad - hoc moves
+        vec![
+            Move::new(self, board, (self.position.0 - 2, self.position.1 - 1)),
+            Move::new(self, board, (self.position.0 - 2, self.position.1 + 1)),
+            Move::new(self, board, (self.position.0 + 2, self.position.1 - 1)),
+            Move::new(self, board, (self.position.0 + 2, self.position.1 + 1)),
+            Move::new(self, board, (self.position.0 - 1, self.position.1 - 2)),
+            Move::new(self, board, (self.position.0 - 1, self.position.1 + 2)),
+            Move::new(self, board, (self.position.0 + 1, self.position.1 - 2)),
+            Move::new(self, board, (self.position.0 + 1, self.position.1 + 2)),
+        ]
     }
 
     fn emoji_repr(&self) -> &'static str {
@@ -147,12 +298,22 @@ impl PieceCommon for Rook {
             position: p,
         }
     }
+
     fn color(&self) -> Color {
         self.color.clone()
     }
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move> {
-        vec![]
+    fn position(&self) -> Position {
+        self.position.clone()
+    }
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move> {
+        let mut moves = vec!();
+        moves.extend(line_left(self, board));
+        moves.extend(line_right(self, board));
+        moves.extend(line_up(self, board));
+        moves.extend(line_down(self, board));
+        moves
     }
 
     fn emoji_repr(&self) -> &'static str {
@@ -180,8 +341,23 @@ impl PieceCommon for Queen {
         self.color.clone()
     }
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move> {
-        vec![]
+    fn position(&self) -> Position {
+        self.position.clone()
+    }
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move> {
+        let mut moves = vec!();
+        // lines
+        moves.extend(line_left(self, board));
+        moves.extend(line_right(self, board));
+        moves.extend(line_up(self, board));
+        moves.extend(line_down(self, board));
+        // diags
+        moves.extend(diag_left_up(self, board));
+        moves.extend(diag_right_up(self, board));
+        moves.extend(diag_left_down(self, board));
+        moves.extend(diag_right_down(self, board));
+        moves
     }
 
     fn emoji_repr(&self) -> &'static str {
@@ -209,8 +385,17 @@ impl PieceCommon for Bishop {
         self.color.clone()
     }
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move> {
-        vec![]
+    fn position(&self) -> Position {
+        self.position.clone()
+    }
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move> {
+        let mut moves = vec!();
+        moves.extend(diag_left_up(self, board));
+        moves.extend(diag_right_up(self, board));
+        moves.extend(diag_left_down(self, board));
+        moves.extend(diag_right_down(self, board));
+        moves
     }
 
     fn emoji_repr(&self) -> &'static str {
@@ -234,12 +419,29 @@ impl PieceCommon for King {
             position: p,
         }
     }
+
     fn color(&self) -> Color {
         self.color.clone()
     }
 
-    fn gen_moves(&self, board: ChessBoard) -> Vec<Move> {
-        vec![]
+    fn position(&self) -> Position {
+        self.position.clone()
+    }
+
+    fn gen_moves(&self, board: &ChessBoard) -> Vec<Move> {
+        // Ad-hoc moves
+        vec![
+            // Lines
+            Move::new(self, board, (self.position.0, self.position.1 + 1)),
+            Move::new(self, board, (self.position.0, self.position.1 - 1)),
+            Move::new(self, board, (self.position.0 + 1, self.position.1)),
+            Move::new(self, board, (self.position.0 + 1, self.position.1)),
+            // Diags
+            Move::new(self, board, (self.position.0 + 1, self.position.1 + 1)),
+            Move::new(self, board, (self.position.0 + 1, self.position.1 - 1)),
+            Move::new(self, board, (self.position.0 - 1, self.position.1 + 1)),
+            Move::new(self, board, (self.position.0 - 1, self.position.1 - 1)),
+        ]
     }
 
     fn emoji_repr(&self) -> &'static str {
